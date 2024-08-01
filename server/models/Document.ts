aaa705d7ca512737bdab378bb7fb0ef159c1c79a
Apoorv Mishra <apoorvmishra101092@gmail.com>
2024-08-01 17:51:05 +0530

    fix: disconnect doc from an archived collection when restoring to another collection

--- server/models/Document.ts
+++ server/models/Document.ts
@@ -958,10 +958,13 @@ class Document extends ParanoidModel<
   };
 
   // Restore an archived document back to being visible to the team
-  unarchive = async (user: User, options?: FindOptions) => {
+  restoreTo = async (
+    collectionId: string,
+    options: FindOptions & { user: User }
+  ) => {
     const { transaction } = { ...options };
-    const collection = this.collectionId
-      ? await Collection.findByPk(this.collectionId, {
+    const collection = collectionId
+      ? await Collection.findByPk(collectionId, {
           transaction,
           lock: transaction?.LOCK.UPDATE,
         })
@@ -985,9 +988,6 @@ class Document extends ParanoidModel<
       await collection.addDocumentToStructure(this, undefined, {
         transaction,
       });
-      if (this.collection) {
-        this.collection.documentStructure = collection.documentStructure;
-      }
     }
 
     if (this.deletedAt) {
@@ -995,9 +995,11 @@ class Document extends ParanoidModel<
     }
 
     this.archivedAt = null;
-    this.lastModifiedById = user.id;
-    this.updatedBy = user;
+    this.lastModifiedById = options.user.id;
+    this.updatedBy = options.user;
+    this.collectionId = collectionId;
     await this.save({ transaction });
+    await this.reload({ transaction });
     return this;
   };
 
